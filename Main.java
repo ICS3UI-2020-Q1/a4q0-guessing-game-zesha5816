caaf41a59b9310d8735de060410ddbac71462d65
zesha5816 <zesha5816@wrdsb.ca>
2020-10-02 13:37:50 +0000

    finished the example question

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+import java.util.Random;
 /**
- *
- * @author 
+ * simple number guessing game. The idea is, the computer will pick a number at random. It will then ask you to guess the number. If your guess is too high or too low, it will say that to you as feedback.
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +11,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // scanner input
+		Scanner input = new Scanner(System.in);
+		//creating the random generator
+		Random rand= new Random();
+		//create a random number
+    int randNum = rand.nextInt(100)+1;
+		//promp user for inout
+		int guess;
+		//game loop
+		do{
+				// ask the user for their guess
+				System.out.println("Please guess a number between 1 and 100");
+				guess = input.nextInt();
+				if (guess == randNum){
+					System.out.println("You are correct! Good job!");
+				}else if (guess < randNum){
+					System.out.println("You are too low. Try again!");
+				}else if (guess > randNum){
+					System.out.println("You are too high. Try again.!");
+				}
+		}while(guess != randNum);
+		
   }
 }
